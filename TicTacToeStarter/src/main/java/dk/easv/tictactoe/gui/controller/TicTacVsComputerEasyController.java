a6096ea977ef48007a594d9dfce17b51ce14990f
ISMAIL\SNOOP <ismahm01@easv365.dk>
2025-10-09 13:23:30 +0200

    no message

--- TicTacToeStarter/src/main/java/dk/easv/tictactoe/gui/controller/TicTacVsComputerEasyController.java
+++ TicTacToeStarter/src/main/java/dk/easv/tictactoe/gui/controller/TicTacVsComputerEasyController.java
@@ -6,8 +6,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Paint;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Random;
 
 public class TicTacVsComputerEasyController extends TicTac2playerController implements Initializable {
@@ -73,36 +71,5 @@ public class TicTacVsComputerEasyController extends TicTac2playerController impl
 
             playComputer();
         }
-        /*
-        Object[] buttonList = gridPane.getChildren().toArray();
-
-        int min = 0; int max = buttonList.length;
-        int nullCount = 0;
-        for (Button[] row: board) {
-            for (Button btn: row) {
-                if (btn == null) {
-                    nullCount++;
-                    System.out.println(nullCount);
-                }
-            }
-        }
-
-        Button randomBtn = (Button) buttonList[random.nextInt(max-min) + min];
-        if (randomBtn.getText().isEmpty() && nullCount != 0) {
-            for (Button[] rows: board) {
-                List<Button> btnList = Arrays.asList(rows);
-                if (!btnList.contains(randomBtn)) {
-                    System.out.println("contains button");
-                    Integer row = GridPane.getRowIndex(randomBtn);
-                    Integer col = GridPane.getColumnIndex(randomBtn);
-                    int r = (row == null) ? 0 : row;
-                    int c = (col == null) ? 0 : col;
-                    board[r][c] = randomBtn;
-                    randomBtn.setText("O");
-                    randomBtn.setTextFill(Paint.valueOf("Blue"));
-                    break;
-                }
-            }
-        } else if (!randomBtn.getText().isEmpty() && nullCount != 0) {playComputer();}*/
     }
 }
